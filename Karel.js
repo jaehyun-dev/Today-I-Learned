0f0a70fbe12e67e18e8c753e88ea3ab5b03568d7
jaehyun-dev <71001479+jaehyun-dev@users.noreply.github.com>
2022-01-23 22:50:24 +0900

    Update Karel.js

--- Karel.js
+++ Karel.js
@@ -1,3 +1,77 @@
+# 2022.01.23
+
+//Unit 8 Lesson 5
+
+//Make Karel fill the world
+//with beepers
+function main() {
+   //your code here
+   while(leftIsClear()){
+      putBeeperLine();
+      moveToAnotherLine();
+   }
+   putBeeperLine();
+}
+
+function putBeeperLine(){
+   putBeeper();
+   while(frontIsClear()) {
+      move();
+      putBeeper();
+   }
+}
+
+function moveToAnotherLine(){
+   turnAround();
+   while(frontIsClear()){
+      move();
+   }
+   turnRight();
+   move();
+   turnRight();
+}
+
+//Unit 9 Lesson 2
+
+//Karel must help rebuild 
+//broken columns. Make a 
+//column of beepers above
+//each beeper you find on
+//the first row
+function main() {
+   while(frontIsClear()){
+      move();
+      if(beepersPresent()){
+         putBeeperColumn();
+      }
+   }
+}
+
+function putBeeperColumn() {
+   turnLeft();
+   putBeeperLine();
+   turnAround();
+   moveToWall();
+   turnLeft();
+}
+
+function putBeeperLine() {
+   while(frontIsClear()){
+      move();
+      putBeeper();
+   }
+}
+
+function moveToWall() {
+   while(frontIsClear()) {
+      move();
+   }
+}
+
+
+
+# 2022.01.18
+
 //Unit 12 Lesson 1
 
 //Your final task is to teach
