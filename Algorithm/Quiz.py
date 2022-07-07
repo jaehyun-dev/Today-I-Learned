c560b1254366a887759c427ae194b007380b3989
jaehyun-dev <71001479+jaehyun-dev@users.noreply.github.com>
2022-07-08 04:03:55 +0900

    Update Quiz.py

--- Algorithm/Quiz.py
+++ Algorithm/Quiz.py
@@ -197,3 +197,39 @@ def trapping_rain(buildings):
 # 테스트
 print(trapping_rain([3, 0, 0, 2, 0, 4]))
 print(trapping_rain([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
+
+
+
+# 22.07.08 04:03
+
+# 
+
+def consecutive_sum(start, end):
+    # 코드를 작성하세요
+
+    # base case: start와 end가 같으면 start값 반환하기
+    if start == end:
+        return start
+    
+    # recursive case: 절반씩 분할하여 정복하기.
+    return consecutive_sum(start, (start + end) // 2) + consecutive_sum((start + end) // 2 + 1, end)
+
+# def consecutive_sum(start, end):
+#     # base case   
+#     if start == end:
+#         return start
+    
+#     # 부분 문제를 반으로 나눠주기 위해서 문제의 정중앙을 정의한다 (Divide)
+#     mid = (start + end) // 2
+    
+#     # 각 부분 문제를 재귀적으로 풀고(Conquer), 부분 문제의 답을 서로 더한다(Combine).
+#     return consecutive_sum(start, mid) + consecutive_sum(mid + 1, end)
+
+# 테스트
+print(consecutive_sum(1, 10))
+print(consecutive_sum(1, 100))
+print(consecutive_sum(1, 253))
+print(consecutive_sum(1, 388))
+
+
+ 
