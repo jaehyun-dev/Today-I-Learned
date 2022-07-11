4d2af7686a440f00c0f8ba208ea8b97531da2ca5
jaehyun-dev <71001479+jaehyun-dev@users.noreply.github.com>
2022-07-11 23:16:17 +0900

    Update Quiz.py

--- Algorithm/Quiz.py
+++ Algorithm/Quiz.py
@@ -684,3 +684,118 @@ def fib_tab(n):
 print(fib_tab(10))
 print(fib_tab(56))
 print(fib_tab(132))
+
+
+# 22.07.11 22:08
+
+# Fibonacci Sequence with Optimized Space Complexity
+
+def fib_optimized(n):
+    # 코드를 작성하세요. 
+    previous = 0
+    current = 1
+    i = 0
+    while i < n - 1:
+        current, previous = previous + current, current
+        i += 1
+    return current
+
+# 모범답안
+# def fib_optimized(n):
+#     current = 1
+#     previous = 0
+
+#     # 반복적으로 위 변수들을 업데이트한다. 
+#     for i in range(1, n):
+#         current, previous = current + previous, current
+
+#     # n번재 피보나치 수를 리턴한다. 
+#     return current
+
+
+# 테스트
+print(fib_optimized(16))
+print(fib_optimized(53))
+print(fib_optimized(213))
+
+
+
+# Max Profit
+### Memoization 익숙지 않은 듯
+### 고민 깊게 하지 못하고 해설 봄
+
+def max_profit_memo(price_list, count, cache):
+    # 코드를 작성하세요.
+    # base case
+    if count < 2:
+        cache[count] = price_list[count]
+        return cache[count]
+    
+    # 이미 count개의 최대 수익이 계산됐으면:
+    # 저장된 값을 바로 리턴한다
+    if count in cache:
+        return cache[count]
+        
+    # 아직 count개의 최대 수익이 계산되지 않았으면:
+    # 계산을 한 후 cache에 저장 
+    max_profit_count = 0
+    for i in range(1, count // 2 + 1):
+        max_profit_count = max(max_profit_count, max_profit_memo(price_list, i, cache) + max_profit_memo(price_list, count - i, cache))
+        
+    cache[count] = max_profit_count
+    
+    if count < len(price_list):
+        cache[count] = max(cache[count], price_list[count])
+    
+    return cache[count]
+    
+def max_profit(price_list, count):
+    max_profit_cache = {}
+
+    return max_profit_memo(price_list, count, max_profit_cache)
+
+
+# 모범답안
+# def max_profit_memo(price_list, count, cache):
+#     # Base Case: 0개 혹은 1개면 부분 문제로 나눌 필요가 없기 때문에 가격을 바로 리턴한다
+#     if count < 2:
+#         cache[count] = price_list[count]
+#         return cache[count]
+
+#     # 이미 계산한 값이면 cache에 저장된 값을 리턴한다
+#     if count in cache:
+#         return cache[count]
+
+#     # profit은 count개를 팔아서 가능한 최대 수익을 저장하는 변수
+#     # 팔려고 하는 총개수에 대한 가격이 price_list에 없으면 일단 0으로 설정
+#     # 팔려고 하는 총개수에 대한 가격이 price_list에 있으면 일단 그 가격으로 설정
+#     if count < len(price_list):
+#         profit = price_list[count]
+#     else:
+#         profit = 0
+
+#     # count개를 팔 수 있는 조합들을 비교해서, 가능한 최대 수익을 profit에 저장
+#     for i in range(1, count // 2 + 1):
+#         profit = max(profit, max_profit_memo(price_list, i, cache) 
+#                  + max_profit_memo(price_list, count - i, cache))
+
+#     # 계산된 최대 수익을 cache에 저장
+#     cache[count] = profit
+
+#     return profit
+
+
+# def max_profit(price_list, count):
+#     max_profit_cache = {}
+
+#     return max_profit_memo(price_list, count, max_profit_cache)
+
+
+
+# 테스트
+print(max_profit([0, 100, 400, 800, 900, 1000], 5))
+print(max_profit([0, 100, 400, 800, 900, 1000], 10))
+print(max_profit([0, 100, 400, 800, 900, 1000, 1400, 1600, 2100, 2200], 9))
+
+
+
