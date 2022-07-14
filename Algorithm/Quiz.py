2ae6af444de7e80f31c8eede8cd9dd749f3a33b4
jaehyun-dev <71001479+jaehyun-dev@users.noreply.github.com>
2022-07-15 01:08:41 +0900

    Update Quiz.py

--- Algorithm/Quiz.py
+++ Algorithm/Quiz.py
@@ -801,30 +801,53 @@ print(max_profit([0, 100, 400, 800, 900, 1000, 1400, 1600, 2100, 2200], 9))
 
 
 
-# 22.07.14 23:22
+# 22.07.15 01:07
 
 # Max Profit by Tabulation
-# 아직 잘 작동하지 않음
-# 더 고민해봐야 할 듯
 def max_profit(price_list, count):
     # 코드를 작성하세요.
     max_profit_list = []
-    
-    for i in range(0, 2):
+    for i in range(len(price_list)):
         max_profit_list.append(price_list[i])
     
-    if count < 2:
-        return price_list[count]
-
+    for i in range(count - len(price_list) + 1):
+        max_profit_list.append(0)
+    
     for i in range(2, count + 1):
         max_profit_count = 0
         for j in range(1, i // 2 + 1):
             max_profit_count = max(max_profit_count, max_profit_list[j] + max_profit_list[i - j])
-        max_profit_list.append(max_profit_count)    
+        if max_profit_count > max_profit_list[i]:
+            max_profit_list[i] = max_profit_count    
     return max_profit_list[count]
+
+# 모범답안
+# def max_profit(price_list, count):
+#     # 개수별로 가능한 최대 수익을 저장하는 리스트
+#     # 새꼼달꼼 0개면 0원
+#     profit_table = [0]
+
+#     # 개수 1부터 count까지 계산하기 위해 반복문
+#     for i in range(1, count + 1):
+#         # profit은 count개를 팔아서 가능한 최대 수익을 저장하는 변수
+#         # 팔려고 하는 총개수에 대한 가격이 price_list에 있으면 일단 그 가격으로 설정
+#         # 팔려고 하는 총개수에 대한 가격이 price_list에 없으면 일단 0으로 설정   
+#         if i < len(price_list):
+#             profit = price_list[i]
+#         else:
+#             profit = 0
+
+#         # count개를 팔 수 있는 조합들을 비교해서, 가능한 최대 수익을 찾는다
+#         for j in range(1, i // 2 + 1):
+#             profit = max(profit, profit_table[j] + profit_table[i - j])
+
+#         profit_table.append(profit)
+
+#     return profit_table[count]
 
 # 테스트
 print(max_profit([0, 200, 600, 900, 1200, 2000], 5))
 print(max_profit([0, 300, 600, 700, 1100, 1400], 8))
 print(max_profit([0, 100, 200, 400, 600, 900, 1200, 1300, 1500, 1800], 9))
+
 
