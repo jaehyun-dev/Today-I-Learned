259fc6fc044a357e8fabc88970951b64db949341
jaehyun-dev <71001479+jaehyun-dev@users.noreply.github.com>
2022-07-14 23:22:27 +0900

    Update Quiz.py

--- Algorithm/Quiz.py
+++ Algorithm/Quiz.py
@@ -801,7 +801,7 @@ print(max_profit([0, 100, 400, 800, 900, 1000, 1400, 1600, 2100, 2200], 9))
 
 
 
-# 22.07.12 23:56
+# 22.07.14 23:22
 
 # Max Profit by Tabulation
 # 아직 잘 작동하지 않음
@@ -809,24 +809,22 @@ print(max_profit([0, 100, 400, 800, 900, 1000, 1400, 1600, 2100, 2200], 9))
 def max_profit(price_list, count):
     # 코드를 작성하세요.
     max_profit_list = []
-    for i in range(len(price_list)):
+    
+    for i in range(0, 2):
         max_profit_list.append(price_list[i])
     
     if count < 2:
-        return max_profit_list[count]
+        return price_list[count]
 
-    if count < len(price_list):
-        for i in range(2, count):
-            for j in range(1, i // 2 + 1):
-                max_profit_list[i] = max(max_profit_list[i], max_profit_list[j] + max_profit_list[i - j])
-    for i in  range(len(price_list), count + 1):
+    for i in range(2, count + 1):
+        max_profit_count = 0
         for j in range(1, i // 2 + 1):
-            max_profit_count = 0
             max_profit_count = max(max_profit_count, max_profit_list[j] + max_profit_list[i - j])
-            max_profit_list.append(max_profit_count)
+        max_profit_list.append(max_profit_count)    
     return max_profit_list[count]
 
 # 테스트
 print(max_profit([0, 200, 600, 900, 1200, 2000], 5))
 print(max_profit([0, 300, 600, 700, 1100, 1400], 8))
 print(max_profit([0, 100, 200, 400, 600, 900, 1200, 1300, 1500, 1800], 9))
+
