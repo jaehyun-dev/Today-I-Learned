3db5e2884caf44e3ca67334b652c955620026836
jaehyun-dev <71001479+jaehyun-dev@users.noreply.github.com>
2022-07-23 16:04:24 +0900

    Update Quiz.py

--- Algorithm/Quiz.py
+++ Algorithm/Quiz.py
@@ -1323,3 +1323,78 @@ def sublist_max(profits):
 # 테스트
 print(sublist_max([7, -3, 4, -8]))
 print(sublist_max([-2, -3, 4, -1, -2, 1, 5, -3, -1]))
+
+
+# 22.07.23 15:21
+
+# 삼송전자 주식 분석
+
+def max_profit(stock_list):
+    # 코드를 작성하세요.
+    profit_list = []
+    
+    for i in range(1, len(stock_list)):
+        profit_list.append(stock_list[i] - stock_list[i - 1])
+    
+    max_profit_so_far = profit_list[0]
+    max_check = profit_list[0]
+    
+    for i in range(1, len(profit_list)):
+        max_check = max(max_check + profit_list[i], profit_list[i])
+        max_profit_so_far = max(max_profit_so_far, max_check)
+    
+    return max_profit_so_far
+
+# 투자 귀재 규식이 III 응용함
+
+# 모범답안
+# def max_profit(stock_list):
+#     # 현재까지의 최대 수익
+#     max_profit_so_far = stock_list[1] - stock_list[0]
+
+#     # 현재까지의 최소 주식 가격
+#     min_so_far = min(stock_list[0], stock_list[1])
+
+#     # 주식 가격을 하나씩 확인한다
+#     for i in range(2, len(stock_list)):
+#         # 현재 파는 것이 좋은지 확인한다
+#         max_profit_so_far = max(max_profit_so_far, stock_list[i] - min_so_far)
+
+#         # 현재 주식 가격이 최솟값인지 확인한다
+#         min_so_far = min(min_so_far, stock_list[i])
+
+#     return max_profit_so_far
+
+
+# 테스트
+print(max_profit([7, 1, 5, 3, 6, 4]))
+print(max_profit([7, 6, 4, 3, 1]))
+print(max_profit([11, 13, 9, 13, 20, 14, 19, 12, 19, 13]))
+print(max_profit([12, 4, 11, 18, 17, 19, 1, 19, 14, 13, 7, 15, 10, 1, 3, 6]))
+
+
+# 출근하는 방법 I
+
+def staircase(n):
+    # 코드를 작성하세요.
+    new_list = [0, 1, 1]
+    
+    for i in range(3, n + 2):
+        new_list.append(new_list[i - 2] + new_list[i - 1])
+        
+    return new_list[n + 1]
+
+
+# 모범답안
+# def staircase(n):
+#     a, b = 1, 1
+#     for i in range(n):
+#         a, b = b, a + b
+#     return a    
+
+# 테스트
+print(staircase(0))
+print(staircase(6))
+print(staircase(15))
+print(staircase(25))
+print(staircase(41))
