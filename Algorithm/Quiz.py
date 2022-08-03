5d6a61cfacc44eba8d490f2805cc872ccb42cd44
jaehyun-dev <71001479+jaehyun-dev@users.noreply.github.com>
2022-08-03 23:52:58 +0900

    Update Quiz.py

--- Algorithm/Quiz.py
+++ Algorithm/Quiz.py
@@ -1453,3 +1453,18 @@ print(staircase(5, [1, 2, 3]))
 print(staircase(6, [1, 2, 3]))
 print(staircase(7, [1, 2, 4]))
 print(staircase(8, [1, 3, 5]))
+
+
+# 22.08.03 23:52
+
+# 중복되는 항목 찾기 II
+
+def find_same_number(some_list, start, end):
+    # 필요한 경우, start와 end를 옵셔널 파라미터로 만들어도 됩니다.
+    # 코드를 쓰세요
+
+
+# 중복되는 수 ‘하나’만 리턴합니다.
+print(find_same_number([1, 4, 3, 5, 3, 2]))
+print(find_same_number([4, 1, 5, 2, 3, 5]))
+print(find_same_number([5, 2, 3, 4, 1, 6, 7, 8, 9, 3]))
