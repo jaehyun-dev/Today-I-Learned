209054e57e368182e62350e6ae9aa1a1ea202328
jaehyun-dev <71001479+jaehyun-dev@users.noreply.github.com>
2022-08-07 23:51:48 +0900

    Update Quiz.py

--- Algorithm/Quiz.py
+++ Algorithm/Quiz.py
@@ -1505,7 +1505,7 @@ print(find_same_number([4, 1, 5, 2, 3, 5]))
 print(find_same_number([5, 2, 3, 4, 1, 6, 7, 8, 9, 3]))
 
 
-# 22.08.06 18:08
+# 22.08.07 23:50
 
 # 리스트 항목 합 탐색
 
@@ -1516,6 +1516,26 @@ def sum_in_list(search_sum, sorted_list):
             if sorted_list[i] + sorted_list[j] == search_sum:
                 return True
     return False
+
+# 모범답안
+# def sum_in_list(search_sum, sorted_list):
+#     low = 0
+#     high = len(sorted_list) - 1
+    
+#     while low < high:
+#         candidate_sum = sorted_list[low] + sorted_list[high]
+        
+#         if candidate_sum == search_sum: # 합이 찾으려는 숫자일 때
+#             return True
+        
+#         if candidate_sum < search_sum:  # 합이 찾으려는 숫자보다 작을 때
+#             low += 1
+        
+#         else: # 합이 찾으려는 숫자보다 클 때
+#             high -= 1
+    
+#     # 찾는 조합이 없기 때문에 False 리턴
+#     return False
 
 print(sum_in_list(15, [1, 2, 5, 6, 7, 9, 11]))
 print(sum_in_list(15, [1, 2, 5, 7, 9, 11]))
