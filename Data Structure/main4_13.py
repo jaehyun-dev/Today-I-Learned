06f97f23648e57d3ec2c8025d6b235eb1d73dfea
jaehyun-dev <71001479+jaehyun-dev@users.noreply.github.com>
2023-01-05 15:21:50 +0900

    Add files via upload

---
+++ Data Structure/main4_13.py
@@ -0,0 +1,13 @@
+class Node:
+    """링크드 리스트 노드"""
+    def __init__(self, data):
+        self.data = data  # 실제 노드가 저장하는 데이터
+        self.next = None  # 다음 노드에 대한 레퍼런스
+        self.prev = None  # 이전 노드에 대한 레퍼런스
+
+
+class LinkedList:
+    """링크드 리스트 클래스"""
+    def __init__(self):
+        self.head = None  # 링크드 리스트의 가장 앞 노드
+        self.tail = None  # 링크드 리스트의 가장 뒤 노드
