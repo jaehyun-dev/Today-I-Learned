d52ee29be7870e482cba3b373e5e7c98a8fb3807
jaehyun-dev <71001479+jaehyun-dev@users.noreply.github.com>
2023-01-05 15:53:46 +0900

    Add files via upload

---
+++ Data Structure/main4_17.py
@@ -0,0 +1,107 @@
+class Node:
+    """더블리 링크드 리스트 노드"""
+    def __init__(self, data):
+        self.data = data  # 실제 노드가 저장하는 데이터
+        self.next = None  # 다음 노드에 대한 레퍼런스
+        self.prev = None  # 이전 노드에 대한 레퍼런스
+
+
+class LinkedList:
+    """더블리 링크드 리스트 클래스"""
+    def __init__(self):
+        self.head = None  # 링크드 리스트의 가장 앞 노드
+        self.tail = None  # 링크드 리스트의 가장 뒤 노드
+
+
+    def insert_after(self, previous_node, data):
+        """더블리 링크드 리스트 삽입 연산"""
+        new_node = Node(data)
+
+        # tail노드 뒤에 삽입할 때
+        if previous_node is self.tail:
+            LinkedList.append(self, data)
+        # 두 노드 사이에 삽입할 때
+        else:
+            new_node.prev = previous_node
+            new_node.next = previous_node.next
+            previous_node.next = new_node
+            new_node.next.prev = new_node
+
+
+    def append(self, data):
+        """링크드 리스트 추가 연산 메소드"""
+        new_node = Node(data)  # 새로운 데이터를 저장하는 노드
+
+        # 링크드 리스트가 비어 있는 경우
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        # 링크드 리스트에 이미 노드가 있는 경우
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+
+
+    def find_node_at(self, index):
+        """링크드 리스트 접근 연산 메소드. 파라미터 인덱스는 항상 있다고 가정한다"""
+
+        iterator = self.head  # 링크드 리스트를 돌기 위해 필요한 노드 변수
+
+        # index 번째 있는 노드로 간다
+        for _ in range(index):
+            iterator = iterator.next
+
+        return iterator
+
+    def find_node_with_data(self, data):
+        """링크드 리스트에서 주어진 데이터를 갖고있는 노드를 리턴한다. 단, 해당 노드가 없으면 None을 리턴한다"""
+        iterator = self.head  # 링크드 리스트를 돌기 위해 필요한 노드 변수
+
+        while iterator is not None:
+            if iterator.data == data:
+                return iterator
+
+            iterator = iterator.next
+
+        return None
+
+    def __str__(self):
+        """링크드 리스트를 문자열로 표현해서 리턴하는 메소드"""
+        res_str = "|"
+
+        # 링크드 리스트 안에 모든 노드를 돌기 위한 변수. 일단 가장 앞 노드로 정의한다.
+        iterator = self.head
+
+        # 링크드 리스트 끝까지 돈다
+        while iterator is not None:
+            # 각 노드의 데이터를 리턴하는 문자열에 더해준다
+            res_str += " {} |".format(iterator.data)
+            iterator = iterator.next  # 다음 노드로 넘어간다
+
+        return res_str
+
+
+# 빈 링크드 리스트 정의
+my_list = LinkedList()
+
+# 링크드 리스트에 데이터 추가
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+my_list.append(7)
+
+print(my_list)
+# | 2 | 3 | 5 | 7 |
+
+node_7 = my_list.find_node_with_data(7)
+my_list.insert_after(node_7, 11)
+
+print(my_list)
+#| 2 | 3 | 5 | 7 | 11 |
+
+node_2 = my_list.find_node_with_data(2)
+my_list.insert_after(node_2, 13)
+
+print(my_list)
+# | 2 | 13 | 3 | 5 | 7 | 11 |
