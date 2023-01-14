5bfa9f39d60e6bd9197fd9c672b8d2be17cbf2e6
jaehyun-dev <71001479+jaehyun-dev@users.noreply.github.com>
2023-01-14 22:38:42 +0900

    Update main6_12.py

--- Data Structure/1 Basic Data Structures/6 Abstract Data Type/main6_12.py
+++ Data Structure/1 Basic Data Structures/6 Abstract Data Type/main6_12.py
@@ -8,18 +8,23 @@ def parentheses_checker(string):
     stack = deque() # 사용할 스택 정의
 
     # 여기에 코드를 작성하세요
+    # 문자열의 각 문자를 돌면서
     for i in range(len(string)):
+        # 열리는 괄호가 있는 위치를 스택에 저장한다
         if string[i] == "(":
             stack.append(i)
-        if string[i] == ")":
-            if stack and string[stack[-1]] == "(":
+        # 닫히는 괄호가 있으면
+        elif string[i] == ")":
+            # 스택에 열린 괄호 위치 데이터가 있으면 삭제하고
+            if stack:
                 stack.pop()
+            # 아니면 현재 위치에 있는 닫는 괄호에 맞는 열리는 괄호가 없다고 출력한
             else:
                 print(f"문자열 {i} 번째 위치에 있는 닫는 괄호에 맞는 열리는 괄호가 없습니다")
+
+    # 스택에 열린 괄호 위치 데이터가 남아 있으면 해당 열린 괄호는 짝이 맞는 닫힌 괄호가 없다는 뜻이다
     while stack:
-        i = stack.pop()
-        if string[i] == "(":
-            print(f"문자열 {i} 번째 위치에 있는 괄호가 닫히지 않았습니다")
+        print(f"문자열 {stack.pop()} 번째 위치에 있는 괄호가 닫히지 않았습니다")
 
 
 
@@ -36,3 +41,20 @@ parentheses_checker(case3)
 parentheses_checker(case4)
 parentheses_checker(case5)
 parentheses_checker(case6)
+
+
+
+''' 모범답안 보기 전 작성 코드
+for i in range(len(string)):
+    if string[i] == "(":
+        stack.append(i)
+    if string[i] == ")":
+        if stack and string[stack[-1]] == "(":
+            stack.pop()
+        else:
+            print(f"문자열 {i} 번째 위치에 있는 닫는 괄호에 맞는 열리는 괄호가 없습니다")
+while stack:
+    i = stack.pop()
+    if string[i] == "(":
+        print(f"문자열 {i} 번째 위치에 있는 괄호가 닫히지 않았습니다")
+'''
